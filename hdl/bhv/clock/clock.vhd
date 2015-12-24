b9f26d69f845238ca89245697e52df694e265f43
Marc Pignat <marc@pignat.org>
2015-12-24 16:46:09 +0100

    bhv clock

---
+++ hdl/bhv/clock/clock.vhd
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------------
+-- file		: clock.vhd 
+--
+-- brief		: Clock generator (for simulation)
+-- author(s)	: marc at pignat dot org
+-- license		: The MIT License (MIT) (http://opensource.org/licenses/MIT)
+--				  Copyright (c) 2015 Marc Pignat
+-----------------------------------------------------------------------------
+
+library ieee;
+	use ieee.std_logic_1164.all;
+	use ieee.numeric_std.all;
+
+entity clock is
+	generic
+	(
+		frequency	: real	:= 1.0e6
+	);
+	port
+	(
+		clock		: out	std_ulogic
+	);
+end clock;
+
+architecture bhv of clock is
+	constant period : time := (1.0 / frequency) * (1 sec);
+begin
+
+clock_gen : process is
+begin
+	clock <= '0';
+	wait for period / 2;
+	clock <= '1';
+	wait for period / 2;
+end process;
+
+end architecture bhv;
