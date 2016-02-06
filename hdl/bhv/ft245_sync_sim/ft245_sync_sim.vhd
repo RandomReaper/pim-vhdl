f413eeadda3a173035307d87d062eaf856de9c1b
Marc Pignat <marc@pignat.org>
2016-02-06 16:53:13 +0100

    ft245_sync_sim: better simulation

--- hdl/bhv/ft245_sync_sim/ft245_sync_sim.vhd
+++ hdl/bhv/ft245_sync_sim/ft245_sync_sim.vhd
@@ -26,6 +26,7 @@ entity ft245_sync_sim is
 		suspend_n	: out	std_ulogic;
 		
 		d_data_out	: out	std_ulogic_vector(7 downto 0);
+		d_data_out_valid : out std_ulogic;
 		d_data_in	: in	std_ulogic_vector(7 downto 0);
 		d_data_write: in	std_ulogic;
 		d_data_full	: out	std_ulogic
@@ -38,30 +39,28 @@ architecture bhv of ft245_sync_sim is
 	signal rd			: std_ulogic;
 	signal data_in		: std_ulogic_vector(adbus'range);
 	signal data_in_pre	: std_ulogic_vector(adbus'range);
-	signal data_in_in	: std_ulogic_vector(adbus'range);
 	signal d_data_out_in	: std_ulogic_vector(adbus'range);
 	signal clock		: std_ulogic;
 	signal tx_full		: std_ulogic;
 	signal status_empty	: std_ulogic;
-	signal status_empty_in	: std_ulogic;
 	signal status_full	: std_ulogic;
 	signal rd_empty		: std_ulogic;
-	signal rd_old		: std_ulogic;
 	signal wr			: std_ulogic;
 	signal tx_enable	: std_ulogic;
 	signal tx_enable_old: std_ulogic;
 	signal tx_fifo_empty: std_ulogic;
 	signal txe			: std_ulogic;
+	signal tx			: std_ulogic;
 begin
 
 oe			<= not oe_n;
-wr			<= not wr_n and txe;
+wr			<= not wr_n and txe and not tx_full;
 clkout		<= clock;
 rxf_n		<= status_empty;
-txe_n		<= not txe;
+txe_n		<= not txe or tx_full;
 suspend_n	<= '0';
 d_data_full <= status_full;
-
+tx			<= tx_enable and not tx_fifo_empty;
 tx_counter_gen: process(reset, clock)
 begin
 	if reset = '1' then
@@ -69,7 +68,7 @@ begin
 		txe			<= '0';
 		tx_enable_old<= '0';
 	elsif rising_edge(clock) then
-		tx_enable_old<= tx_enable;
+		tx_enable_old<= tx;
 		if tx_full = '1' then
 			tx_enable <= '1';
 			txe		  <= '0';
@@ -93,7 +92,7 @@ i_clock: entity work.clock
 		clock	=> clock
 	);
 	
-i_from_host_fifo: entity work.fifo
+i_from_host_fifo: entity work.fifo_preread
 generic map
 (
 	g_depth_log2 => 4
@@ -110,21 +109,24 @@ port map
 
 	-- outputs
 	read		=> rd,
-	read_data	=> data_in_in,
+	read_data	=> data_in,
 
 	--status
 	status_full	=> status_full,
-	status_empty	=> status_empty_in
+	status_empty	=> status_empty
 );
 
 data_out_gen: process(reset, clock)
 begin
 	if reset = '1' then
 		d_data_out	<= (others => '-');
+		d_data_out_valid <= '0';
 	elsif rising_edge(clock) then
 		d_data_out	<= (others => '-');
-		if tx_enable = '1' and tx_enable_old = '1' then
+		d_data_out_valid <= '0';
+		if tx_enable_old = '1' then
 			d_data_out <= d_data_out_in;
+			d_data_out_valid <= '1';
 		end if;
 	end if;
 end process;
@@ -145,7 +147,7 @@ port map
 	write_data	=> std_ulogic_vector(adbus),
 
 	-- outputs
-	read		=> tx_enable,
+	read		=> tx,
 	read_data	=> d_data_out_in,
 
 	--status
@@ -153,24 +155,7 @@ port map
 	status_empty	=> tx_fifo_empty
 );
 
-iface:process(reset, clock)
-begin
-	if reset = '1' then
-		data_in_pre <= (others => '0');
-		status_empty <= '1';
-		rd_old <= '0';
-	elsif rising_edge(clock) then
-		rd_old <= not rd_n;
-		if rd_old = '1' then
-			data_in_pre <= data_in_in;
-		end if;
-		status_empty <= status_empty_in;
-	end if;
-end process;
-
-data_in <= data_in_in when rd_old = '1' else data_in_pre;
-rd_empty <= not(status_empty_in) and status_empty;
-rd <= rd_empty or not rd_n;
+rd <= not rd_n and not status_empty;
 
 i_reset : entity work.reset
 port map
