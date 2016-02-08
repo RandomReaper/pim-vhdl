325fcb718572ebde62bf80e39e3dc58387f8a965
Marc Pignat <marc@pignat.org>
2016-02-08 20:15:39 +0100

    width_changer: rtl + tb

--- hdl/rtl/width_changer/width_changer.vhd
+++ hdl/rtl/width_changer/width_changer.vhd
@@ -16,11 +16,6 @@ library ieee;
 	use ieee.numeric_std.all;
 
 entity width_changer_internal is
-	generic
-	(
-		g_width_in		: positive := 3;
-		g_width_out		: positive := 1
-	);
 	port
 	(
 		clock			: in	std_ulogic;
@@ -29,6 +24,7 @@ entity width_changer_internal is
 		in_data			: in	std_ulogic_vector;
 		in_data_valid	: in	std_ulogic;
 		in_data_ready	: out	std_ulogic;
+		
 		out_data		: out	std_ulogic_vector;
 		out_data_valid	: out	std_ulogic
 	);
@@ -39,11 +35,6 @@ library ieee;
 	use ieee.numeric_std.all;
 
 entity width_changer is
-	generic
-	(
-		g_width_in		: positive := 3;
-		g_width_out		: positive := 1
-	);
 	port
 	(
 		clock			: in	std_ulogic;
@@ -52,6 +43,7 @@ entity width_changer is
 		in_data			: in	std_ulogic_vector;
 		in_data_valid	: in	std_ulogic;
 		in_data_ready	: out	std_ulogic;
+		
 		out_data		: out	std_ulogic_vector;
 		out_data_valid	: out	std_ulogic
 	);
@@ -67,8 +59,8 @@ begin
 		and (out_data'left > 0)
 	report "Unsupported feature, feel free to improve this code" severity failure;
 
-smaller: if out_data'left < in_data'left generate
-	assert (in_data'left mod out_data'left) = 0 report "width_changer smaller : modulo size failed" severity failure;
+smaller: if out_data'length < in_data'length generate
+	assert (in_data'length mod out_data'length) = 0 report "width_changer smaller : modulo size failed" severity failure;
 
 	i_smaller: entity work.width_changer_internal(rtl_smaller)
 	port map
@@ -84,8 +76,8 @@ smaller: if out_data'left < in_data'left generate
 	);
 end generate;
 
-bigger: if out_data'left > in_data'left generate
-	assert (out_data'left mod in_data'left) = 0 report "width_changer bigger : modulo size failed" severity failure;
+bigger: if out_data'length > in_data'length generate
+	assert (out_data'length mod in_data'length) = 0 report "width_changer bigger : modulo size failed" severity failure;
 	i_bigger: entity work.width_changer_internal(rtl_bigger)
 	port map
 	(
@@ -109,11 +101,88 @@ end generate;
 end rtl;
 
 architecture rtl_smaller of width_changer_internal is
-	signal memory	: std_ulogic_vector(in_data'range);
+	signal memory		: std_ulogic_vector((in_data'length - out_data'length) -1 downto 0);
+	signal state		: std_ulogic_vector((in_data'length/out_data'length) - 2 downto 0);
 begin
+
+state_proc: process(reset, clock)
+begin
+	if reset = '1' then
+		state <= (others => '0');
+		memory <= (others => '0');
+	elsif rising_edge(clock) then
+		state <= std_ulogic_vector(unsigned(state) srl 1);
+		if in_data_valid = '1' then
+			state <= (state'left => '1', others => '0');
+			memory <= in_data(memory'range);
+			
+			assert unsigned(state) = 0 report "in_data_valid while not empty" severity warning;
+
+		end if;
+	end if;
+end process;
+
+process(state, in_data, in_data_valid, memory)
+begin
+	out_data_valid <= in_data_valid;
+	out_data <= in_data(in_data'left downto in_data'left - out_data'left);
+	in_data_ready <= not in_data_valid;
+	
+	for i in state'range loop
+		if state(i) = '1' then
+			out_data_valid <= '1';
+			in_data_ready <= '0';
+			out_data <= memory(((i+1)*out_data'length) - 1 downto (i+0)*out_data'length);
+		end if;
+	end loop;
+
+	if state(0) = '1' then
+		in_data_ready <= '1';
+	end if;
+
+end process;
+
 end rtl_smaller;
 
 architecture rtl_bigger of width_changer_internal is
-	signal memory	: std_ulogic_vector(out_data'range);
+	signal memory		: std_ulogic_vector(out_data'left downto in_data'left + 1);
+	signal state		: std_ulogic_vector((out_data'length/in_data'length) - 1  downto 0);
+	signal state_changed: std_ulogic;
 begin
+
+in_data_ready <= '1';
+
+state_proc: process(reset, clock)
+begin
+	if reset = '1' then
+		state <= (state'left => '1', others => '0');
+		state_changed <= '0';
+	elsif rising_edge(clock) then
+		state_changed <= '0';
+		if in_data_valid = '1' then
+			state <= std_ulogic_vector(unsigned(state) ror 1);
+			state_changed <= '1';
+		end if;
+	end if;
+end process;
+
+data_proc: process(reset, clock)
+begin
+	if reset = '1' then
+		memory(out_data'left downto in_data'left+1) <= (others => '-');
+	elsif rising_edge(clock) then
+		if in_data_valid = '1' then
+			for i in state'left downto 1 loop
+				if state(i) = '1' then
+					memory(((i+1)*in_data'length) - 1 downto (i+0)*in_data'length) <= in_data;
+				end if;
+			end loop;
+		end if;
+	end if;
+end process;
+
+out_data(in_data'range) <= in_data;
+out_data(out_data'left downto in_data'left + 1) <= memory;
+out_data_valid <= in_data_valid when state(0) = '1' and in_data_valid = '1' else '0';
+
 end rtl_bigger;
--- tb/width_changer/hdl/tb.vhd
+++ tb/width_changer/hdl/tb.vhd
@@ -15,19 +15,70 @@ entity tb is
 end tb;
 
 architecture bhv of tb is
+	constant clock_frequency: real := 100.0e6;
+	--constant clock_period	: time := 1.0 * sec/clock_frequency;
+
 	signal reset			: std_ulogic;
 	signal clock			: std_ulogic;
+	signal stop				: std_ulogic;
 	
-	signal data4			: unsigned(3 downto 0);
-	signal data12			: std_ulogic_vector(11 downto 0);
-	signal data12_valid		: std_ulogic;
-	signal data6			: std_ulogic_vector(5 downto 0);
-	signal data6_valid		: std_ulogic;	
-	signal data24			: std_ulogic_vector(23 downto 0);
-	signal data24_valid		: std_ulogic;	
+	signal data4			: std_ulogic_vector(3 downto 0);
+	signal data4_valid		: std_ulogic;
+	signal data16			: std_ulogic_vector(15 downto 0);
+	signal data16_valid		: std_ulogic;
+	signal data8			: std_ulogic_vector(7 downto 0);
+	signal data8_valid		: std_ulogic;	
+	signal data32			: std_ulogic_vector(31 downto 0);
+	signal data32_valid		: std_ulogic;	
 	signal data4_bis		: std_ulogic_vector(3 downto 0);
-	signal data4_bis_valid	: std_ulogic;	
+	signal data4_bis_valid	: std_ulogic;
+	
+	signal counter			: unsigned(data4'range);
+	signal delay			: unsigned(data4'range);
 begin
+
+data4 <= std_ulogic_vector(counter);
+
+process
+begin
+	stop <= '0';
+	data4_valid <= '0';
+	wait until falling_edge(reset);
+	wait until rising_edge(clock);
+	
+	counter <= (others => '0');
+	data4_valid <= '1';
+	wait until rising_edge(clock);
+
+	delay <= (others => '0');
+	while data4_bis_valid /= '1' loop
+
+		wait until rising_edge(clock);
+
+		counter <= counter + 1;
+
+		delay <= delay + 1;
+
+		assert delay < 10 report "timeout waiting for data4_bis_valid" severity failure;
+		
+	end loop;
+	
+	assert unsigned(data4_bis) = 0 report "wrong data" severity failure;
+
+	for i in 0 to 100 loop
+		counter <= counter + 1;
+		
+		wait until rising_edge(clock);
+		
+		assert counter = unsigned(data4_bis)+delay report "wrong_data" severity failure;
+
+	end loop;
+	
+	stop <= '1';
+	
+	wait;
+
+end process;
 
 i_dut0: entity work.width_changer
 port map
@@ -35,11 +86,11 @@ port map
 	reset			=> reset,
 	clock			=> clock,
 	
-	in_data			=> std_ulogic_vector(data4),
-	in_data_valid	=> '1',
+	in_data			=> data4,
+	in_data_valid	=> data4_valid,
 	in_data_ready	=> open,
-	out_data		=> data12,
-	out_data_valid	=> data12_valid
+	out_data		=> data16,
+	out_data_valid	=> data16_valid
 );
 
 i_dut1: entity work.width_changer
@@ -48,11 +99,11 @@ port map
 	reset			=> reset,
 	clock			=> clock,
 	
-	in_data			=> data12,
-	in_data_valid	=> data12_valid,
+	in_data			=> data16,
+	in_data_valid	=> data16_valid,
 	in_data_ready	=> open,
-	out_data		=> data6,
-	out_data_valid	=> data6_valid
+	out_data		=> data8,
+	out_data_valid	=> data8_valid
 );
 
 i_dut2: entity work.width_changer
@@ -61,11 +112,11 @@ port map
 	reset			=> reset,
 	clock			=> clock,
 	
-	in_data			=> data6,
-	in_data_valid	=> data6_valid,
+	in_data			=> data8,
+	in_data_valid	=> data8_valid,
 	in_data_ready	=> open,
-	out_data		=> data24,
-	out_data_valid	=> data24_valid
+	out_data		=> data32,
+	out_data_valid	=> data32_valid
 );
 
 i_dut3: entity work.width_changer
@@ -74,30 +125,22 @@ port map
 	reset			=> reset,
 	clock			=> clock,
 	
-	in_data			=> data24,
-	in_data_valid	=> data24_valid,
+	in_data			=> data32,
+	in_data_valid	=> data32_valid,
 	in_data_ready	=> open,
 	out_data		=> data4_bis,
 	out_data_valid	=> data4_bis_valid
 );
 
-counter: process(reset, clock)
-begin
-	if reset = '1' then
-		data4 <= (others => '0');
-	elsif rising_edge(clock) then
-		data4 <= data4+1;
-	end if;
-end process;
-
-i_clock : entity work.clock
+i_clock : entity work.clock_stop
 generic map
 (
-	frequency	=> 80.0e6
+	frequency	=> clock_frequency
 )
 port map
 (
-	clock	=> clock
+	clock	=> clock,
+	stop	=> stop
 );
 
 i_reset : entity work.reset
