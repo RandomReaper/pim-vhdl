0fcdae34dcd217c8b63fcab41928bf0cef31d4ca
Marc Pignat <marc@pignat.org>
2016-02-11 08:34:23 +0100

    License changed from MIT to Apache version 2.0
    Probably nothing to do with Richard Stallman conference in Sierre yesterday...

--- hdl/rtl/ft245_sync_if/ft245_sync_if.vhd
+++ hdl/rtl/ft245_sync_if/ft245_sync_if.vhd
@@ -3,8 +3,20 @@
 --
 -- brief		: Interface for FTDI ft2232h in ft245 synchronous mode
 -- author(s)	: marc at pignat dot org
--- license		: The MIT License (MIT) (http://opensource.org/licenses/MIT)
---				  Copyright (c) 2015 Marc Pignat
+-----------------------------------------------------------------------------
+-- Copyright 2015,2016 Marc Pignat
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- 		http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-----------------------------------------------------------------------------
 --
 -- FIXME : loses bytes when the ft245 tx fifo is full
 --
