624cd5eb0267d7a727a1c6836146f539793f9a6f
Marc Pignat <marc@pignat.org>
2016-05-11 10:37:46 +0200

    ghdl scripts update (cygwin compatibility and less verbose all.sh)

--- tb/all.sh
+++ tb/all.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-TEST=`readlink -m ./ghdl-sim.sh`
-BASE=`readlink -m .`
+TEST=$(readlink -m ./ghdl-sim.sh)
+BASE=$(readlink -m .)
 
 find . -name 'project.xise' -print | sed -e 's/project.xise//' | while read dir; do
-	echo "Processing dir '$dir'"
 	cd $dir
-	$TEST
+	if $TEST 2>&1 | grep 'Simulation Done' > /dev/null
+	then
+		echo "success : $dir"
+	else
+		echo "FAILURE : $dir"
+	fi
 	cd $BASE
 done
--- tb/ghdl-sim.sh
+++ tb/ghdl-sim.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-DIR=`mktemp -d`
+DIR=$(mktemp -d)
 GHDL="ghdl"
 OPTIONS="--workdir=$DIR"
 
-echo "workdir=$DIR"
+if uname | grep CYGWIN > /dev/null
+then
+	OPTIONS="--workdir=$(cygpath -w $DIR)"
+	echo $OPTIONS
+fi
+
 cat project.xise | grep "FILE_VHDL" | grep "file xil_pn" | sed -e "s/    <file xil_pn:name=\"//" | sed -e "s/\" xil_pn:type=\"FILE_VHDL\">//" | while read line
 do
-	echo adding file "$line"
 	$GHDL -i $OPTIONS "$line"
 done
 
@@ -14,5 +18,4 @@ $GHDL -m $OPTIONS tb
 $GHDL -e $OPTIONS tb
 $GHDL -r $OPTIONS tb --ieee-asserts=disable-at-0
 rm -r $DIR
-
 
