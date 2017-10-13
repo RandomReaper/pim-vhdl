7ec3f4887ebc07033a15a54ea0455ebc3bf4216f
Marc Pignat <marc@pignat.org>
2017-10-13 13:11:52 +0200

    fixes for current travis-ci image

--- .travis/tb.sh
+++ .travis/tb.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 ORIG=$PWD
-export PATH=$HOME/ghdl/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 source bin/env.sh
 RESULT=$?
 
--- bin/env.sh
+++ bin/env.sh
@@ -8,26 +8,27 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$CURRENT_DIR:$PATH
 if ! ghdl -v >/dev/null 2>&1
 then
-	if ! [ -e $CURRENT_DIR/.cache/ghdl/bin/ghdl ]
+	if ! [ -e $CURRENT_DIR/.cache/ghdl/bin/ghdl ] || ! [ -e $HOME/lib/libgnat-4.6.so.1 ]
 	then
 		>&2 echo '***************************************************************************'
-		>&2 echo '*** WARNING: ghdl not found, trying to install it in \'$CURRENT_DIR/.cache\'
+		>&2 echo '*** WARNING: ghdl not found, trying to install it in $CURRENT_DIR/.cache'
 		>&2 echo '***************************************************************************'
 		HOME=$CURRENT_DIR/.cache $CURRENT_DIR/../.travis/deps.sh || exit 1
 	fi
 
 	export PATH=$CURRENT_DIR/.cache/ghdl/bin:$PATH
 
+	if uname | grep Linux > /dev/null
+	then
+		export LD_LIBRARY_PATH=$CURRENT_DIR/.cache/lib:$LD_LIBRARY_PATH
+	fi
+
+	echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 	if ! ghdl -v
 	then
 		>&2 echo '***************************************************************************'
 		>&2 echo '*** WARNING: ghdl cannot be run'
 		>&2 echo '***************************************************************************'
 		exit 2
-	fi
-
-	if uname | grep Linux > /dev/null
-	then
-		export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 	fi
 fi
