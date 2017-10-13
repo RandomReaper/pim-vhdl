7982a5c867dc94f9b273bb80160d7fbff005e51f
Marc Pignat <marc@pignat.org>
2017-10-13 10:23:46 +0200

    more error checking

--- .travis/tb.sh
+++ .travis/tb.sh
@@ -4,6 +4,13 @@ ORIG=$PWD
 export PATH=$HOME/ghdl/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 source bin/env.sh
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+    echo "script failed, aborting"
+fi
+
+
 cd tb
 all.sh
 RESULT=$?
--- bin/env.sh
+++ bin/env.sh
@@ -11,12 +11,20 @@ then
 	if ! [ -e $CURRENT_DIR/.cache/ghdl/bin/ghdl ]
 	then
 		>&2 echo '***************************************************************************'
-		>&2 echo '*** WARNING: ghdl not found, trying to install it in '$CURRENT_DIR/.cache
+		>&2 echo '*** WARNING: ghdl not found, trying to install it in \'$CURRENT_DIR/.cache\'
 		>&2 echo '***************************************************************************'
-		HOME=$CURRENT_DIR/.cache $CURRENT_DIR/../travis/deps.sh
+		HOME=$CURRENT_DIR/.cache $CURRENT_DIR/../.travis/deps.sh || exit 1
 	fi
 
 	export PATH=$CURRENT_DIR/.cache/ghdl/bin:$PATH
+
+	if ! ghdl -v
+	then
+		>&2 echo '***************************************************************************'
+		>&2 echo '*** WARNING: ghdl cannot be run'
+		>&2 echo '***************************************************************************'
+		exit 2
+	fi
 
 	if uname | grep Linux > /dev/null
 	then
